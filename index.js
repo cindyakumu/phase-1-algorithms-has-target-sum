d11ab2cc6262e8090602a92c74762158ec0055b3
Cindy <cindyakumu40@gmail.com>
2024-07-19 08:50:44 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+  
+  for (let num of array) {
+    const complement = target - num;
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+    seenNumbers.add(num);
+  }
+  
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // true
+console.log(hasTargetSum([1, 2, 5], 4)); // false
+console.log(hasTargetSum([], 5)); // false
+console.log(hasTargetSum([5], 5)); // false
+console.log(hasTargetSum([1, 2, 3, 4, 5], 9)); // true
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
